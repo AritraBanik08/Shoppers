42f0544fdeeb7b5cc16bf1a3b8a63eef3ccac8dd
AritraBanik08 <banikaritra4@gmail.com>
2024-02-11 20:59:48 +0530

    14th Commit

--- src/routes/thankyou.jsx
+++ src/routes/thankyou.jsx
@@ -18,7 +18,7 @@ export default function Home() {
 
               <div class="col-12 mb-md-0 col-md-4 order-md-2 order-1 mb-3 text-center">
                 <div class="site-logo">
-                  <a href="/" class="js-logo-clone">
+                  <a href="index.html" class="js-logo-clone">
                     Shoppers
                   </a>
                 </div>
@@ -38,7 +38,7 @@ export default function Home() {
                       </a>
                     </li>
                     <li>
-                      <a href="/cart" class="site-cart">
+                      <a href="cart.html" class="site-cart">
                         <span class="icon icon-shopping_cart"></span>
                         <span class="count">2</span>
                       </a>
@@ -61,7 +61,7 @@ export default function Home() {
           <div class="container">
             <ul class="site-menu js-clone-nav d-none d-md-block">
               <li class="has-children">
-                <a href="/">Home</a>
+                <a href="index.html">Home</a>
                 <ul class="dropdown">
                   <li>
                     <a href="#">Menu One</a>
@@ -89,7 +89,7 @@ export default function Home() {
                 </ul>
               </li>
               <li class="has-children">
-                <a href="/about">About</a>
+                <a href="about.html">About</a>
                 <ul class="dropdown">
                   <li>
                     <a href="#">Menu One</a>
@@ -103,7 +103,7 @@ export default function Home() {
                 </ul>
               </li>
               <li>
-                <a href="/shop">Shop</a>
+                <a href="shop.html">Shop</a>
               </li>
               <li>
                 <a href="#">Catalogue</a>
@@ -111,8 +111,8 @@ export default function Home() {
               <li>
                 <a href="#">New Arrivals</a>
               </li>
-              <li class="active">
-                <a href="/contact">Contact</a>
+              <li>
+                <a href="contact.html">Contact</a>
               </li>
             </ul>
           </div>
@@ -123,7 +123,7 @@ export default function Home() {
         <div class="container">
           <div class="row">
             <div class="col-md-12 mb-0">
-              <a href="/">Home</a> <span class="mx-2 mb-0">/</span>{" "}
+              <a href="index.html">Home</a> <span class="mx-2 mb-0">/</span>{" "}
               <strong class="text-black">Contact</strong>
             </div>
           </div>
@@ -133,115 +133,15 @@ export default function Home() {
       <div class="site-section">
         <div class="container">
           <div class="row">
-            <div class="col-md-12">
-              <h2 class="h3 mb-3 text-black">Get In Touch</h2>
-            </div>
-            <div class="col-md-7">
-              <form action="#" method="post">
-                <div class="p-lg-5 border p-3">
-                  <div class="form-group row">
-                    <div class="col-md-6">
-                      <label for="c_fname" class="text-black">
-                        First Name <span class="text-danger">*</span>
-                      </label>
-                      <input
-                        type="text"
-                        class="form-control"
-                        id="c_fname"
-                        name="c_fname"
-                      />
-                    </div>
-                    <div class="col-md-6">
-                      <label for="c_lname" class="text-black">
-                        Last Name <span class="text-danger">*</span>
-                      </label>
-                      <input
-                        type="text"
-                        class="form-control"
-                        id="c_lname"
-                        name="c_lname"
-                      />
-                    </div>
-                  </div>
-                  <div class="form-group row">
-                    <div class="col-md-12">
-                      <label for="c_email" class="text-black">
-                        Email <span class="text-danger">*</span>
-                      </label>
-                      <input
-                        type="email"
-                        class="form-control"
-                        id="c_email"
-                        name="c_email"
-                        placeholder=""
-                      />
-                    </div>
-                  </div>
-                  <div class="form-group row">
-                    <div class="col-md-12">
-                      <label for="c_subject" class="text-black">
-                        Subject{" "}
-                      </label>
-                      <input
-                        type="text"
-                        class="form-control"
-                        id="c_subject"
-                        name="c_subject"
-                      />
-                    </div>
-                  </div>
-
-                  <div class="form-group row">
-                    <div class="col-md-12">
-                      <label for="c_message" class="text-black">
-                        Message{" "}
-                      </label>
-                      <textarea
-                        name="c_message"
-                        id="c_message"
-                        cols="30"
-                        rows="7"
-                        class="form-control"
-                      ></textarea>
-                    </div>
-                  </div>
-                  <div class="form-group row">
-                    <div class="col-lg-12">
-                      <input
-                        type="submit"
-                        class="btn btn-primary btn-lg btn-block"
-                        value="Send Message"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </form>
-            </div>
-            <div class="col-md-5 ml-auto">
-              <div class="mb-3 border p-4">
-                <span class="d-block text-primary h6 text-uppercase">
-                  New York
-                </span>
-                <p class="mb-0">
-                  203 Fake St. Mountain View, San Francisco, California, USA
-                </p>
-              </div>
-              <div class="mb-3 border p-4">
-                <span class="d-block text-primary h6 text-uppercase">
-                  London
-                </span>
-                <p class="mb-0">
-                  203 Fake St. Mountain View, San Francisco, California, USA
-                </p>
-              </div>
-              <div class="mb-3 border p-4">
-                <span class="d-block text-primary h6 text-uppercase">
-                  Canada
-                </span>
-                <p class="mb-0">
-                  203 Fake St. Mountain View, San Francisco, California, USA
-                </p>
-              </div>
+            <div class="col-md-12 text-center">
+              <span class="icon-check_circle display-3 text-success"></span>
+              <h2 class="display-3 text-black">Thank you!</h2>
+              <p class="lead mb-5">You order was successfuly completed.</p>
+              <p>
+                <a href="shop.html" class="btn btn-sm btn-primary">
+                  Back to shop
+                </a>
+              </p>
             </div>
           </div>
         </div>
